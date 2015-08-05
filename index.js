3f6436ea73fe47f480c9a97f16f0889138cab497
Evan Lucas <evanlucas@me.com>
2015-08-04 22:18:53 -0500

    add 'use strict'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = function(str) {
   if (!str) return []
   var out = []
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var test = require('tap').test
   , argsplit = require('./')
 
